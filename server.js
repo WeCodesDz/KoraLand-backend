66214c146a2db4fd4ae847deb47f9dba979ccc9f
Oussama Amine Bentama <oa.bentama@gmail.com>
2023-01-14 12:04:03 +0100

    Update server.js
    ffffffffffffff

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ Evaluation.belongsTo(Student, {onDelete: 'cascade'});
   })();
   const app = require('./app');
   
-  
+  ////////////
   
   const server = app.listen(port, () => console.log(`Listening on ${port}`));
   //const server = app.listen();
