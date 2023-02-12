06ad8c058dc8918473a666b5253c62ac71efe0db
Oussama Amine Bentama <oa.bentama@gmail.com>
2023-02-12 13:00:23 +0100

    adding auth
    auth by refresh token and access token (token rotation)

--- src/auth/authController.js
+++ src/auth/authController.js
@@ -7,42 +7,10 @@ const catchAsync = require('../utils/catchAsync');
 const Admin = require('../administrateur/administrateurModel');
 const Coach = require('../coach/coachModel');
 const Parent = require('../parent/parentModel');
+const RefreshAdmin = require('../refreshAdmin/refreshAdminModel');
+const RefreshCoach = require('../refreshCoach/refreshCoachModel');
+const RefreshParent = require('../refreshParent/refreshParentModel');
 
-
-const { Op } = Sequelize;
-
-const signToken = (id,accesToken) =>
-  jwt.sign({ id,accesToken }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
-
-const createSendToken = (user, accesToken, statusCode, res) => {
-  const token = signToken(user.id,accesToken);
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-  res.cookie('jwt', token, cookieOptions);
-
-  // Remove password from output
-  user.password = undefined;
-  //user.passwordConfirm = undefined;
-  user.passwordChangedAt = undefined;
-  user.passwordResetToken = undefined;
-  user.passwordResetExpires = undefined;
-
-  res.status(statusCode).json({
-    status: 'success',
-    token,
-    data: {
-      user,
-    },
-  });
-};
 
 exports.role = (...roles) => {
     return (req, res, next) => {
@@ -55,32 +23,169 @@ exports.role = (...roles) => {
       next();
     };
   };
+ 
+exports.loginAdmin = catchAsync(async (req, res, next) => {
 
-exports.login = catchAsync(async (req, res, next) => {
-    const { email, password,accesToken,username } = req.body;
-    let authenticate;
-    // if(!email ) {
-    //     throw new  AppError('Please provide email ', 400);
-    // }
-    if(!password ) {
-        throw new  AppError('Please provide password ', 400);
-    }
-    if(accesToken === 'admin') 
-    authenticate = await Admin.findOne({ where: { email: email.trim() } });
-    if(accesToken === 'coach')
-    authenticate = await Coach.findOne({ where: { email: email.trim() } });
-    if(accesToken === 'parent')
-    authenticate = await Parent.findOne({ where: { username: username.trim() } });
+  const cookies = req.cookies;
+
+    const {  password,username } = req.body;
     
-    if (!authenticate || !(await authenticate.correctPassword(password.trim(), authenticate.password))) {
-        return next(new AppError('Incorrect Email or password', 401));
+    if(!password || !username) {
+        throw new  AppError('Please provide username and password  ', 400);
+    }
+    
+    const user = await Admin.findOne({ where: { username: username.trim() } });
+    const isCorrectPassword = await user?.correctPassword(password.trim(), user.password);
+
+    
+    if (!user ||
+       !isCorrectPassword) {
+        return next(new AppError('Incorrect username or password', 401));
       }
-     // 3) If everything ok, send token to client
-  createSendToken(authenticate,accesToken, 200, res);  
+     
+      // create JWTs
+      const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": user.username,
+                "role": "admin"
+            }
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '15m' }
+    );
+
+    const newRefreshToken = jwt.sign(
+      { "username": user.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '7d' }
+  );
+
+
+  if(cookies?.jwt){
+    const refreshToken = cookies.jwt;
+    const foundToken = await user.getRefreshAdmins({where:{jwt:refreshToken}});
+    if (!foundToken) {
+     await RefreshAdmin.destroy({where:{administrateurId:user.id}});
+    }
+
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+
+  }
+  const createdRefreshToken = await RefreshAdmin.create({jwt:newRefreshToken});
+  res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 7*24 * 60 * 60 * 1000 });
+
+  res.json({ role:'admin', accessToken });
+
+});  
+
+exports.loginCoach = catchAsync(async (req, res, next) => {
+   const cookies = req.cookies;
+
+    const {  password,username } = req.body;
+    
+    if(!password || !username) {
+        throw new  AppError('Please provide username and password  ', 400);
+    }
+    
+    const user = await Coach.findOne({ where: { username: username.trim() } });
+    const isCorrectPassword = await user?.correctPassword(password.trim(), user.password);
+
+    
+    if (!user ||
+       !isCorrectPassword) {
+        return next(new AppError('Incorrect username or password', 401));
+      }
+     
+      // create JWTs
+      const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": user.username,
+                "role": "coach"
+            }
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '15m' }
+    );
+
+    const newRefreshToken = jwt.sign(
+      { "username": user.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '7d' }
+  );
+
+
+  if(cookies?.jwt){
+    const refreshToken = cookies.jwt;
+    const foundToken = await user.getRefreshCoachs({where:{jwt:refreshToken}});
+    if (!foundToken) {
+     await RefreshCoach.destroy({where:{coachId:user.id}});
+    }
+
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+
+  }
+  const createdRefreshToken = await RefreshCoach.create({jwt:newRefreshToken});
+  res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 7*24 * 60 * 60 * 1000 });
+
+  res.json({ role:'coach', accessToken });
+});  
+
+exports.loginParent = catchAsync(async (req, res, next) => {
+  const cookies = req.cookies;
+
+    const {  password,username } = req.body;
+    
+    if(!password || !username) {
+        throw new  AppError('Please provide username and password  ', 400);
+    }
+    
+    const user = await Parent.findOne({ where: { username: username.trim() } });
+    const isCorrectPassword = await user?.correctPassword(password.trim(), user.password);
+
+    
+    if (!user ||
+       !isCorrectPassword) {
+        return next(new AppError('Incorrect username or password', 401));
+      }
+     
+      // create JWTs
+      const accessToken = jwt.sign(
+        {
+            "UserInfo": {
+                "username": user.username,
+                "role": "parent"
+            }
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '15m' }
+    );
+
+    const newRefreshToken = jwt.sign(
+      { "username": user.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '7d' }
+  );
+
+
+  if(cookies?.jwt){
+    const refreshToken = cookies.jwt;
+    const foundToken = await user.getRefreshParents({where:{jwt:refreshToken}});
+    if (!foundToken) {
+     await RefreshParent.destroy({where:{parentId:user.id}});
+    }
+
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+
+  }
+  const createdRefreshToken = await RefreshParent.create({jwt:newRefreshToken});
+  res.cookie('jwt', newRefreshToken, { httpOnly: true, secure: true, sameSite: 'None', maxAge: 7*24 * 60 * 60 * 1000 });
+
+  res.json({ role:'parent', accessToken });
 });  
 
 exports.protect = catchAsync(async (req, res, next) => {
-    // 1) getting the token and check iff its there
   let token;
   if (
     req.headers.authorization &&
@@ -93,37 +198,55 @@ exports.protect = catchAsync(async (req, res, next) => {
       new AppError('You are not logged in! Please log in to get access.', 401)
     );
   }
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  if(decoded.accesToken === 'admin'){
-    currentUser = await Admin.findByPk(decoded.id);
-    if (!currentUser) {
-     return next(
-       new AppError('Admin belonging to this token does no longer exist.', 401)
-     );
-   }
-    res.locals.user = currentUser;
-    req.user = currentUser;
+
+  const decoded = await promisify(jwt.verify)(token, process.env.ACCESS_TOKEN_SECRET);
+  let currentUser;
+  if(decoded.role.includes('admin') ){
+    currentUser = await Admin.findOne(decoded.username);
  }
- if(decoded.accesToken === 'coach'){
-   currentUser = await Coach.findByPk(decoded.id);
+ if(decoded.role.includes('coach') ){
+   currentUser = await Coach.findOne(decoded.username);
+ }
+ if(decoded.role.includes('parent')){
+   currentUser = await Parent.findOne(decoded.username);
+  }
+   
    if (!currentUser) {
      return next(
-       new AppError('Coach belonging to this token does no longer exist.', 401)
+       new AppError('User belonging to this token does no longer exist.', 401)
      );
    }
-   res.locals.user = currentUser;
    req.user = currentUser;
+   req.role = decoded.role;
+   next();
+  
+});
+
+exports.logout =  catchAsync(async (req, res, next) => {
+  
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); //No content
+  const refreshToken = cookies.jwt;
+
+  // Is refreshToken in db?
+  const decoded = await promisify(jwt.verify)(refreshToken, process.env.ACCESS_TOKEN_SECRET);
+  let currentRefreshToken;
+  if(decoded.role.includes('admin') ){
+    currentRefreshToken = await RefreshAdmin.findOne({where:{jwt:refreshToken}});
  }
- if(decoded.accesToken === 'parent'){
-   currentUser = await Parent.findByPk(decoded.id);
-   if (!currentUser) {
-     return next(
-       new AppError('Parent belonging to this token does no longer exist.', 401)
-     );
-   }
-   res.locals.user = currentUser;
-   req.user = currentUser;
-}
-next(); 
+ if(decoded.role.includes('coach') ){
+  currentRefreshToken = await RefreshCoach.findOne({where:{jwt:refreshToken}});
+ }
+ if(decoded.role.includes('parent')){
+  currentRefreshToken = await RefreshParent.findOne({where:{jwt:refreshToken}});
+  }
+  
+  if (currentRefreshToken) {
+    await currentRefreshToken.destroy();
+  }
+
+
+  res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
+  res.sendStatus(204);
 });
 
