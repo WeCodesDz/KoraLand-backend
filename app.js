5020b974703ff0c9733a20aecafc751691211f99
Oussama Amine Bentama <oa.bentama@gmail.com>
2023-02-23 21:44:02 +0100

    fixes

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ['http://127.0.0.1:5173','http://localhost:5173'],
+    origin: ['http://127.0.0.1:5173','http://localhost:5173','https://koralandacad.link'],
     credentials: true,
     withCredentials: true,
   })
