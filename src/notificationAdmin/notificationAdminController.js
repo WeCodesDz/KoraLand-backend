783abfd71179086ff09515f4a45ea4176392b4b1
Oussama Amine Bentama <oa.bentama@gmail.com>
2023-03-01 23:20:01 +0100

    f

--- src/notificationAdmin/notificationAdminController.js
+++ src/notificationAdmin/notificationAdminController.js
@@ -22,9 +22,7 @@ exports.sendPushNotificationToAdmin = async (admins, notification) => {
     console.log("------------------------------------------");
     console.log("------------------------------------------");
     const newAdmins = await Promise.all(
-      admins.map(async (admin) => 
-        await Admin.findByPk(admin);
-      )
+      admins.map(async (admin) => await Admin.findByPk(admin))
     );
     console.log("------------------------------------------");
     console.log("------------------------------------------");
