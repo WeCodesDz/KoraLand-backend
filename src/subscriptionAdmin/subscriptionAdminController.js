d59b067e256743bdccd546b83d059f3ddc4889b0
Oussama Amine Bentama <oa.bentama@gmail.com>
2023-03-01 23:31:34 +0100

    f

--- src/subscriptionAdmin/subscriptionAdminController.js
+++ src/subscriptionAdmin/subscriptionAdminController.js
@@ -24,7 +24,7 @@ exports.saveSubscription = catchAsync(async (req, res, next) => {
     body: req.body.subscription,
   });
 
-  await newSubscription.addSubs(admin);
+  await newSubscription.addSub(admin);
   res.status(201).json({
     status: "success",
     data: {
