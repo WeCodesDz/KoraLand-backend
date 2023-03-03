c195771e01ebba2ccdfa03cafb8caa99fb1fa2ea
Oussama Amine Bentama <oa.bentama@gmail.com>
2023-03-03 16:59:04 +0100

    f

--- src/notificationAdmin/notificationAdminController.js
+++ src/notificationAdmin/notificationAdminController.js
@@ -67,7 +67,7 @@ exports.getMyNotifications = catchAsync(async (req, res, next) => {
   console.log(id)
   console.log('----------------------------------------------------------------admin------')
 
-  const notifications = await admin.getNotificationAdmin();
+  const notifications = await admin.getNotifications();
   if (!notifications) {
     throw new appError("No notifications found", 404);
   }
