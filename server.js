cbeaaf588520fbb1885b794d950763755f47582c
xenn1337 <m.oubouabdellah@gmail.com>
2023-03-04 13:08:53 +0100

    debug

--- server.js
+++ server.js
@@ -200,7 +200,7 @@ const server = app.listen(port, () => console.log(`Listening on ${port}`));
 const httpServer = http.createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "*",
+    origin: ['http://127.0.0.1:5173','http://localhost','http://localhost:5173','https://koralandacad.link'],
     methods: ["GET", "POST"],
     // credentials: true,
     // withCredentials: true,
