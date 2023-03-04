eb21bf708c0c5e52e8a30e990804995c3c0624c5
xenn1337 <m.oubouabdellah@gmail.com>
2023-03-04 13:11:08 +0100

    debug

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ const io = require("socket.io")(httpServer, {
   cors: {
     origin: ['http://127.0.0.1:5173','http://localhost','http://localhost:5173','https://koralandacad.link'],
     methods: ["GET", "POST"],
-    // credentials: true,
+    credentials: true,
     // withCredentials: true,
   },
 });
