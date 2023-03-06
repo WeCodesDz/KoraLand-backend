e284174c2e45957069b815b5f067039a0e2caa1b
xenn1337 <m.oubouabdellah@gmail.com>
2023-03-06 13:23:00 +0100

    debug

--- socket.js
+++ socket.js
@@ -91,6 +91,7 @@ module.exports = {
 
       nodeEventEmitter.on("send_new_evaluation",async (data) => {
         console.log('---------------------------------- in send new evaluation emmitter')
+        console.log('data',data)
         const admins = await Administrateur.findAll({
           attributes:['username','id'],
           where:{
