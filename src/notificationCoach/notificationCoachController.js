c095c98155eda08749f28d4194e80500f687ecee
xenn1337 <m.oubouabdellah@gmail.com>
2023-03-07 11:05:26 +0100

    fixes

--- src/notificationCoach/notificationCoachController.js
+++ src/notificationCoach/notificationCoachController.js
@@ -54,7 +54,7 @@ exports.createNotificationCoach = async (coachs, notif) => {
   //we add validations after
   const notification = await NotificationCoach.create(notif);
   console.log(Object.getPrototypeOf(notification));
-  await notification.setCoachs(coachs);
+  await notification.setCoaches(coachs);
 
   //return something
   return notification;
